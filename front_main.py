6f98f8526e62aed9455cd37bfb03641a215e7911
Vladimir Sotnikov <vladimir.sotnikov@jetbrains.com>
2022-06-13 16:01:26 +0300

    fix front

--- front_main.py
+++ front_main.py
@@ -1,5 +1,6 @@
 import requests
 import streamlit as st
+
 from result_image_reqs import req_img_name
 
 st.set_page_config(
@@ -8,14 +9,17 @@ st.set_page_config(
     layout='wide',
     initial_sidebar_state='expanded',
 )
-st.markdown("""<img src="https://zora.co/assets/og-image.png" alt="drawing" width="500"/>""", unsafe_allow_html=True)
+st.markdown(
+    """<img src="https://zora.co/assets/og-image.png" alt="drawing" width="500"/>""",
+    unsafe_allow_html=True)
 st.title('ZORA-powered NFT AI Scanner')
 social_md = st.markdown(
     '[GitHub](https://github.com/vvsotnikov/zora-hackathon)' '   [Twitter](https://twitter.com/Neural9000/status/1536295324104507393)',
     unsafe_allow_html=True)
-metanft_colab_button = st.markdown('Meta NFT Image Generator available in Colab Jupyter notebook\n\n'
-                                   '[![MetaNFT in Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/vvsotnikov/zora-hackathon/blob/master/MetNFT.ipynb)',
-                                   unsafe_allow_html=True)
+metanft_colab_button = st.markdown(
+    'Meta NFT Image Generator available in Colab Jupyter notebook\n\n'
+    '[![MetaNFT in Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/vvsotnikov/zora-hackathon/blob/master/MetNFT.ipynb)',
+    unsafe_allow_html=True)
 
 col1, col2 = st.columns(2)
 pl1, pl2, pl3 = st.columns(3)
@@ -29,7 +33,8 @@ md6 = pl6.empty()
 
 
 def text_search():
-    search_result = requests.get(f'http://zora-server:8000/find_text?query={search_text_input}').json()
+    search_result = requests.get(
+        f'http://zora-server:8000/find_text?query={search_text_input}').json()
 
     nft1 = req_img_name(search_result[0]["contract"], search_result[0]["token_id"])
     image1 = (
@@ -77,74 +82,86 @@ def text_search():
         f"{image1}"
         f"\n\n{coll_name1}#{token_id1} "
         f"\n\nEtherscan link: https://etherscan.io/token/{coll_addr1}?a={token_id1} "
-        f"\n\nZORA link: https://zora.co/collections/{coll_addr1}/{token_id1}", unsafe_allow_html=True)
+        f"\n\nZORA link: https://zora.co/collections/{coll_addr1}/{token_id1}",
+        unsafe_allow_html=True)
     md2.markdown(
         f"{image2}"
         f"\n\n{coll_name2}#{token_id2} "
         f"\n\nEtherscan link: https://etherscan.io/token/{coll_addr2}?a={token_id2} "
-        f"\n\nZORA link: https://zora.co/collections/{coll_addr2}/{token_id2}", unsafe_allow_html=True)
+        f"\n\nZORA link: https://zora.co/collections/{coll_addr2}/{token_id2}",
+        unsafe_allow_html=True)
     md3.markdown(
         f"{image3}"
         f"\n\n{coll_name3}#{token_id3} "
         f"\n\nEtherscan link: https://etherscan.io/token/{coll_addr3}?a={token_id3} "
-        f"\n\nZORA link: https://zora.co/collections/{coll_addr3}/{token_id3}", unsafe_allow_html=True)
+        f"\n\nZORA link: https://zora.co/collections/{coll_addr3}/{token_id3}",
+        unsafe_allow_html=True)
     md4.markdown(
         f"{image4}"
         f"\n\n{coll_name4}#{token_id4} "
         f"\n\nEtherscan link: https://etherscan.io/token/{coll_addr4}?a={token_id4} "
-        f"\n\nZORA link: https://zora.co/collections/{coll_addr4}/{token_id4}", unsafe_allow_html=True)
+        f"\n\nZORA link: https://zora.co/collections/{coll_addr4}/{token_id4}",
+        unsafe_allow_html=True)
     md5.markdown(
         f"{image5}"
         f"\n\n{coll_name5}#{token_id5} "
         f"\n\nEtherscan link: https://etherscan.io/token/{coll_addr5}?a={token_id5} "
-        f"\n\nZORA link: https://zora.co/collections/{coll_addr5}/{token_id5}", unsafe_allow_html=True)
+        f"\n\nZORA link: https://zora.co/collections/{coll_addr5}/{token_id5}",
+        unsafe_allow_html=True)
     md6.markdown(
         f"{image6}"
         f"\n\n{coll_name6}#{token_id6} "
         f"\n\nEtherscan link: https://etherscan.io/token/{coll_addr6}?a={token_id6} "
-        f"\n\nZORA link: https://zora.co/collections/{coll_addr6}/{token_id6}", unsafe_allow_html=True)
+        f"\n\nZORA link: https://zora.co/collections/{coll_addr6}/{token_id6}",
+        unsafe_allow_html=True)
 
 
 def image_search():
     image_search_result = requests.post(f'http://zora-server:8000/find_image',
                                         files={'image': search_image_input}).json()
 
-    nft1 = req_img_name(image_search_result[0]["contract"], image_search_result[0]["token_id"])
+    nft1 = req_img_name(image_search_result[0]["contract"],
+                        image_search_result[0]["token_id"])
     image1 = (
         f"""<img src="{nft1[1]}" alt="drawing" width="300"/>""")
     coll_name1 = nft1[0]
     token_id1 = image_search_result[0]["token_id"]
     coll_addr1 = image_search_result[0]["contract"]
 
-    nft2 = req_img_name(image_search_result[1]["contract"], image_search_result[1]["token_id"])
+    nft2 = req_img_name(image_search_result[1]["contract"],
+                        image_search_result[1]["token_id"])
     image2 = (
         f"""<img src="{nft2[1]}" alt="drawing" width="300"/>""")
     coll_name2 = nft2[0]
     token_id2 = image_search_result[1]["token_id"]
     coll_addr2 = image_search_result[1]["contract"]
 
-    nft3 = req_img_name(image_search_result[2]["contract"], image_search_result[2]["token_id"])
+    nft3 = req_img_name(image_search_result[2]["contract"],
+                        image_search_result[2]["token_id"])
     image3 = (
         f"""<img src="{nft3[1]}" alt="drawing" width="300"/>""")
     coll_name3 = nft3[0]
     token_id3 = image_search_result[2]["token_id"]
     coll_addr3 = image_search_result[2]["contract"]
 
-    nft4 = req_img_name(image_search_result[3]["contract"], image_search_result[3]["token_id"])
+    nft4 = req_img_name(image_search_result[3]["contract"],
+                        image_search_result[3]["token_id"])
     image4 = (
         f"""<img src="{nft4[1]}" alt="drawing" width="300"/>""")
     coll_name4 = nft4[0]
     token_id4 = image_search_result[3]["token_id"]
     coll_addr4 = image_search_result[3]["contract"]
 
-    nft5 = req_img_name(image_search_result[4]["contract"], image_search_result[4]["token_id"])
+    nft5 = req_img_name(image_search_result[4]["contract"],
+                        image_search_result[4]["token_id"])
     image5 = (
         f"""<img src="{nft5[1]}" alt="drawing" width="300"/>""")
     coll_name5 = nft5[0]
     token_id5 = image_search_result[4]["token_id"]
     coll_addr5 = image_search_result[4]["contract"]
 
-    nft6 = req_img_name(image_search_result[5]["contract"], image_search_result[5]["token_id"])
+    nft6 = req_img_name(image_search_result[5]["contract"],
+                        image_search_result[5]["token_id"])
     image6 = (
         f"""<img src="{nft6[1]}" alt="drawing" width="300"/>""")
     coll_name6 = nft6[0]
@@ -155,40 +172,61 @@ def image_search():
         f"{image1}"
         f"\n\n{coll_name1}#{token_id1} "
         f"\n\nEtherscan link: https://etherscan.io/token/{coll_addr1}?a={token_id1} "
-        f"\n\nZORA link: https://zora.co/collections/{coll_addr1}/{token_id1}", unsafe_allow_html=True)
+        f"\n\nZORA link: https://zora.co/collections/{coll_addr1}/{token_id1}",
+        unsafe_allow_html=True)
     md2.markdown(
         f"{image2}"
         f"\n\n{coll_name2}#{token_id2} "
         f"\n\nEtherscan link: https://etherscan.io/token/{coll_addr2}?a={token_id2} "
-        f"\n\nZORA link: https://zora.co/collections/{coll_addr2}/{token_id2}", unsafe_allow_html=True)
+        f"\n\nZORA link: https://zora.co/collections/{coll_addr2}/{token_id2}",
+        unsafe_allow_html=True)
     md3.markdown(
         f"{image3}"
         f"\n\n{coll_name3}#{token_id3} "
         f"\n\nEtherscan link: https://etherscan.io/token/{coll_addr3}?a={token_id3} "
-        f"\n\nZORA link: https://zora.co/collections/{coll_addr3}/{token_id3}", unsafe_allow_html=True)
+        f"\n\nZORA link: https://zora.co/collections/{coll_addr3}/{token_id3}",
+        unsafe_allow_html=True)
     md4.markdown(
         f"{image4}"
         f"\n\n{coll_name4}#{token_id4} "
         f"\n\nEtherscan link: https://etherscan.io/token/{coll_addr4}?a={token_id4} "
-        f"\n\nZORA link: https://zora.co/collections/{coll_addr4}/{token_id4}", unsafe_allow_html=True)
+        f"\n\nZORA link: https://zora.co/collections/{coll_addr4}/{token_id4}",
+        unsafe_allow_html=True)
     md5.markdown(
         f"{image5}"
         f"\n\n{coll_name5}#{token_id5} "
         f"\n\nEtherscan link: https://etherscan.io/token/{coll_addr5}?a={token_id5} "
-        f"\n\nZORA link: https://zora.co/collections/{coll_addr5}/{token_id5}", unsafe_allow_html=True)
+        f"\n\nZORA link: https://zora.co/collections/{coll_addr5}/{token_id5}",
+        unsafe_allow_html=True)
     md6.markdown(
         f"{image6}"
         f"\n\n{coll_name6}#{token_id6} "
         f"\n\nEtherscan link: https://etherscan.io/token/{coll_addr6}?a={token_id6} "
-        f"\n\nZORA link: https://zora.co/collections/{coll_addr6}/{token_id6}", unsafe_allow_html=True)
+        f"\n\nZORA link: https://zora.co/collections/{coll_addr6}/{token_id6}",
+        unsafe_allow_html=True)
 
 
-search_text_input = col1.text_input('NFT image search by text', help='Enter your dark NFT fantasy here',
-                                    placeholder="pixel art 8 bit")
-if search_text_input:
+def schedule_text_search():
+    st.session_state.run_text_search = True
+
+
+def schedule_image_search():
+    st.session_state.run_image_search = True
+
+
+search_text_input = col1.text_input('NFT image search by text',
+                                    help='Enter your dark NFT fantasy here',
+                                    placeholder="pixel art 8 bit",
+                                    on_change=schedule_text_search)
+
+search_image_input = col2.file_uploader(
+    'NFT image search by image',
+    type=['png', 'jpg', 'jpeg'],
+    help='Load your NFT image to find NFTs with simmilar images',
+    on_change=schedule_image_search)
+if 'run_text_search' in st.session_state and st.session_state.run_text_search and search_text_input:
     text_search()
-
-search_image_input = col2.file_uploader('NFT image search by image', type=['png', 'jpg', 'jpeg'],
-                                        help='Load your NFT image to find NFTs with simmilar images')
-if search_image_input:
+    st.session_state.run_text_search = False
+if 'run_image_search' in st.session_state and st.session_state.run_image_search and search_image_input:
     image_search()
+    st.session_state.run_image_search = False
